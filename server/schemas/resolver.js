05fd02acc4057898336d677df56e4a6ef8c7b3fd
Abby Rosenthal <adlundeen@gmail.com>
2022-09-29 23:19:54 -0500

    addReview resolver written

--- server/schemas/resolver.js
+++ server/schemas/resolver.js
@@ -1,4 +1,4 @@
-const { Services, User } = require('../models')
+const { Service, User, Review } = require('../models')
 const { gql } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
@@ -13,7 +13,7 @@ const resolvers = {
             throw new AuthenticationError('Not logged in');
         },
         //TO DO: use the service id to look for it inside all the user services arrays... the reverse of bonus in project 18 ... use the mongoose $
-        services: 
+        //services: 
     },
 
     Mutation: {
@@ -41,22 +41,23 @@ const resolvers = {
             //return { token, user };
         },
 
-        //TO DO: This needs to be fixed to send service id to args
-        //could just send the _id over in place of args... it is not part of context anymore... that needs to go
-        addReview: async(parent, args, context) => {
+        //TO DO: This needs to be fixed
+        addReview: async (parent, args, context) => {
+            //only allows logged in users to leave reviews
             if (context.user) {
-                const review = await Review.create({...args, username: context.user.username});
-
-                await Service.findByIdAndUpdate(
-                    {_id: context.user._id},
-                    { $push: { reviews: review._id } },
-                    { new: true }
-                );
-
-                return review;
+              const review = await Review.create({ ...args, username: context.user.username });
+      
+              await Service.findByIdAndUpdate(
+                { _id: context.service._id },
+                { $push: { review: review._id } },
+                { new: true }
+              );
+      
+              return review;
             }
+      
             throw new AuthenticationError('You need to be logged in!');
-        },
+          },
 
         addService: {
             // TO DO: have it link to a dropdown with pre-determined job options
