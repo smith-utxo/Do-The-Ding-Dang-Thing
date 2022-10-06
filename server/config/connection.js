a6d99ddd71069b1215e3cf5f672415e01e1c3102
Adam Smith <smith.adam937@gmail.com>
2022-10-06 16:17:39 -0500

    Mongoose uri parameter to openuri must be a string error

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
-
+require ('dotenv').config();
 
 mongoose
   .connect(
-    process.env.MONGODB_URI ||
-      "mongodb://127.0.0.1:27017/do-the-ding-dang-thing",
+    process.env.MONGODB_URI // ||
+     // "mongodb://127.0.0.1:27017/do-the-ding-dang-thing"
+     ,
       console.log(process.env.MONGODB_URI),
     {
       useNewUrlParser: true,
