e4552b07580ada038af78363e303b3926cea34ba
MrTofuuu <chris.stallcup@gmail.com>
2022-10-11 22:13:57 -0500

    updating mongoose package seems to make the ereateIndex issue go away

--- server/seeders/seedGen.js
+++ server/seeders/seedGen.js
@@ -1,59 +1,70 @@
-function generateData() {
-    // Generate 100 random users that fit the User model
-    function generateUsers() {
-      // const serviceArray = 
-  
-      const seedServiceDB = async () => {
-        await Service.deleteMany({});
-        await Service.insertMany(serviceArray);
-      };
-      seedServiceDB().then(() => {
-        console.log("Successfully seeded services!");
-      });
-  
-      for (let i = 0; i < 50; i++) {
-        let username = faker.internet.userName();
-        let email = faker.internet.email(`${username}`);
-        let password = faker.internet.password(12);
-        let phone = faker.phone.number("###-###-####");
-        let services =
-          serviceArray[
-            Math.floor(Math.random() * (serviceArray.length - 0 + 1) + 1)
-          ];
-  
-        users.push({
-          username: username,
-          email: email,
-          password: password,
-          phone: phone,
-          // services: services,
-        });
-      }
-      console.log(users);
-      return users;
-    }
-  
-    generateUsers();
-    // Generate 20 random reviews that follow made by some of the users just created
-    function generateReviews() {
-      for (let i = 0; i < 20; i++) {
-        let reviewBody = faker.lorem.sentences(2);
-        let username = users[Math.floor(Math.random() * users.length)].username;
-        let createdAt = faker.date.recent();
-        let rating = Math.floor(Math.random() * (5 - 1 + 1) + 1);
-  
-        reviews.push({
-          reviewBody: reviewBody,
-          username: username,
-          createdAt: createdAt,
-          rating: rating,
-        });
-      }
-  
-      return reviews;
-    }
-  
-    generateReviews();
+const { faker } = require("@faker-js/faker");
+const { User, Review, Service } = require("../models");
+
+const userGen = () => {
+  const seedServiceDB = async () => {
+    
+    await Service.insertMany(serviceArray);
+  };
+  seedServiceDB().then(() => {
+    console.log("Successfully seeded services!");
+  });
+
+  for (let i = 0; i < 50; i++) {
+    let username = faker.internet.userName();
+    let email = faker.internet.email(`${username}`);
+    let password = faker.internet.password(12);
+    let phone = faker.phone.number("###-###-####");
+    let services =
+      serviceArray[
+        Math.floor(Math.random() * (serviceArray.length - 0 + 1) + 1)
+      ];
+
+    users.push({
+      username: username,
+      email: email,
+      password: password,
+      phone: phone,
+      // services: services,
+    });
   }
+  console.log(users);
+  return users;
+};
+
+const reviewGen = () => {
+  for (let i = 0; i < 20; i++) {
+    let reviewBody = faker.lorem.sentences(2);
+    let username = users[Math.floor(Math.random() * users.length)].username;
+    let createdAt = faker.date.recent();
+    let rating = Math.floor(Math.random() * (5 - 1 + 1) + 1);
+
+    reviews.push({
+      reviewBody: reviewBody,
+      username: username,
+      createdAt: createdAt,
+      rating: rating,
+    });
+  }
+
+  return reviews;
+};
+
+const serviceGen = () => {
+    for (let i = 0; i < 20; i++) {
+      let reviewBody = faker.lorem.sentences(2);
+      let username = users[Math.floor(Math.random() * users.length)].username;
+      let createdAt = faker.date.recent();
+      let rating = Math.floor(Math.random() * (5 - 1 + 1) + 1);
   
-  generateData();
+      reviews.push({
+        reviewBody: reviewBody,
+        username: username,
+        createdAt: createdAt,
+        rating: rating,
+      });
+    }
+  
+    return reviews;
+  };
+module.exports = userGen, reviewGen, serviceGen;
--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,28 +1,96 @@
 const { faker } = require("@faker-js/faker");
-const mongoose = require("mongoose");
-const db = require('../config/connection');
-const seedGen = require('./seedGen')
+// const { User, Review, Service } = require("../models");
+// ---------------------------------------------------------------
+const db = require("../config/connection");
+// const {userGen, reviewGen, serviceGen} = require('./seedGen')
 // const fs = require('fs');
 const { User, Review, Service } = require("../models");
+// ----------------------------------------------------------------
+// const userGen = () => {
+//   const seedServiceDB = async () => {
+//     await Service.insertMany(serviceArray);
+//   };
+//   seedServiceDB().then(() => {
+//     console.log("Successfully seeded services!");
+//   });
 
-db.once('open mongodb connection', async () =>{
-  try{
-  await User.deleteMany({});
-  await Review.deleteMany({});
+//   for (let i = 0; i < 50; i++) {
+//     let username = faker.internet.userName();
+//     let email = faker.internet.email(`${username}`);
+//     let password = faker.internet.password(12);
+//     let phone = faker.phone.number("###-###-####");
+//     let services =
+//       serviceArray[
+//         Math.floor(Math.random() * (serviceArray.length - 0 + 1) + 1)
+//       ];
 
-  // await User.insertMany(users);
-  // await Review.insertMany(reviews);
-  await User.create(userSeeds);
-  await Review.create(reviewSeeds);
-  } catch (err){
+//     users.push({
+//       username: username,
+//       email: email,
+//       password: password,
+//       phone: phone,
+//       // services: services,
+//     });
+//   }
+//   console.log(users);
+//   return users;
+// };
+
+// const reviewGen = () => {
+//   for (let i = 0; i < 20; i++) {
+//     let reviewBody = faker.lorem.sentences(2);
+//     let username = users[Math.floor(Math.random() * users.length)].username;
+//     let createdAt = faker.date.recent();
+//     let rating = Math.floor(Math.random() * (5 - 1 + 1) + 1);
+
+//     reviews.push({
+//       reviewBody: reviewBody,
+//       username: username,
+//       createdAt: createdAt,
+//       rating: rating,
+//     });
+//   }
+
+//   return reviews;
+// };
+
+// const serviceGen = () => {
+//   for (let i = 0; i < 20; i++) {
+//     let reviewBody = faker.lorem.sentences(2);
+//     let username = users[Math.floor(Math.random() * users.length)].username;
+//     let createdAt = faker.date.recent();
+//     let rating = Math.floor(Math.random() * (5 - 1 + 1) + 1);
+
+//     reviews.push({
+//       reviewBody: reviewBody,
+//       username: username,
+//       createdAt: createdAt,
+//       rating: rating,
+//     });
+//   }
+
+//   return reviews;
+// };
+db.once("open", async () => {
+  try {
+    await User.deleteMany({});
+    console.log("Users deleted");
+    // await Review.deleteMany({});
+    // await Service.deleteMany({});
+    // await User.insertMany(users);
+    // await Review.insertMany(reviews);
+
+    // await User.create(userGen);
+    // await Review.create(reviewGen);
+    // await Service.create(serviceGen);
+  } catch (err) {
     console.error(err);
     process.exit(1);
   }
 
-  console.log('Successfully seeded database!');
+  console.log("Successfully seeded database!");
   process.exit(0);
 });
-
 
 // const seedDB = async () => {
 //   await User.deleteMany({});
